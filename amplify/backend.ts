63cb4340af1627ad41b2bdd9ff0c5d9cd887330c
Sterling <sterling1@hotmail.ca>
2026-05-12 19:28:51 -0400

    Readded authentication definition.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,9 @@
 import { defineBackend } from '@aws-amplify/backend';
-// import { data } from './data/resource';
+import { auth } from './auth/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
-  // auth
+  auth
 });
